c561ac635127e96a0bb12b7c16b6a8afe86c2d33
Duilio Ruggiero <duilio@sinetris.net>
2024-03-07 16:43:02 +0100

    Revrite the script to check DNS and certs from linux-desktop

--- platform/ansible/custom_roles/sinetris.bootstrap/files/check-linux-desktop-config.sh
+++ platform/ansible/custom_roles/sinetris.bootstrap/files/check-linux-desktop-config.sh
@@ -2,38 +2,62 @@
 set -Eeuo pipefail
 
 base_domain=iam-demo.test
-check_hostnames_on_port=( \
-  'iam-control-plane 6443' \
-  'git 443' \
-  'grafana 443' \
-  'prometheus 443' \
-  'alertmanager 443' \
-  'consul 443' \
-  'keycloak 443' \
+check_hostnames_on_port=(
+  'Kubernetes-Control-Plane iam-control-plane 6443'
+  'Gitea git 443'
+  'Grafana grafana 443'
+  'Loki loki 443'
+  'Prometheus prometheus 443'
+  'Alertmanager alertmanager 443'
+  'Consul consul 443'
+  'Keycloak keycloak 443'
+  'Hashicorp-Vault vault 443'
+  'MinIO-S3-API s3 443'
+  'Minio-Web-Console minio 443'
 )
 
 bold_text=$(tput bold)
+bad_result_text=$(tput setaf 1)
 good_result_text=$(tput setaf 2)
-bad_result_text=$(tput setaf 3)
-info_result_text=$(tput setaf 4)
+highlight_text=$(tput setaf 3)
+info_text=$(tput setaf 4)
 reset_text=$(tput sgr0)
-for hostname_and_port in "${check_hostnames_on_port[@]}"; do
-  splitted_host_port=( $hostname_and_port )
-  fqdn_to_check=${splitted_host_port[0]}.${base_domain}
-  port_to_check=${splitted_host_port[1]}
-  echo "${info_result_text}Check ${bold_text}${fqdn_to_check}:${port_to_check}${reset_text}"
-  cmd_output=$(echo | openssl s_client -showcerts -servername ${fqdn_to_check} \
-      -connect "${fqdn_to_check}:${port_to_check}" 2>/dev/null \
-      | openssl x509 -inform pem -noout -nocert -checkhost ${fqdn_to_check} 2>/dev/null) \
-    && exit_status=$? || exit_status=$?
-  if [[ $cmd_output =~ "does match certificate" ]]; then
-    echo " ${bold_text}${good_result_text}[OK]${reset_text}"
-    echo "   '${good_result_text}${fqdn_to_check}${reset_text}' on port '${good_result_text}${port_to_check}${reset_text}'"
-  elif [ "${exit_status}" -ne "0" ]; then
+echo "Check DNS entries and Certificates for ${bold_text}${highlight_text}${base_domain}${reset_text}"
+for name_hostname_port in "${check_hostnames_on_port[@]}"; do
+  tmp_splitted_line=($name_hostname_port)
+  service_name=${tmp_splitted_line[0]}
+  fqdn_to_check=${tmp_splitted_line[1]}.${base_domain}
+  port_to_check=${tmp_splitted_line[2]}
+  echo "Check ${info_text}${service_name}${reset_text} on ${info_text}${bold_text}${fqdn_to_check}:${port_to_check}${reset_text}"
+  openssl_connect_output=$(echo | openssl s_client -showcerts -servername ${fqdn_to_check} \
+    -connect "${fqdn_to_check}:${port_to_check}" 2>&1) &&
+    exit_status=$? || exit_status=$?
+  if [ "${exit_status}" -ne "0" ]; then
     echo " ${bold_text}${bad_result_text}[Error]${reset_text}"
-    echo "   ${bad_result_text}Can not check certificate for${reset_text} '${bold_text}${bad_result_text}${fqdn_to_check}:${port_to_check}${reset_text}'"
+    echo "   ${highlight_text}Can not check certificate for${reset_text} '${bold_text}${fqdn_to_check}:${port_to_check}${reset_text}'"
+    if [[ $openssl_connect_output =~ "No client certificate CA names sent" ]]; then
+      echo "   ${bad_result_text}Most likely reason: ${bold_text}no TLS setup on the endpoint${reset_text}"
+    elif [[ $openssl_connect_output =~ "Name or service not known" ]]; then
+      echo "   ${bad_result_text}Most likely reason: ${bold_text}DNS resolution error${reset_text}"
+    elif [[ $openssl_connect_output =~ "Connection refused" ]]; then
+      echo "   ${bad_result_text}Most likely reason: ${bold_text}wrong port number${reset_text}"
+    else
+      echo "   ${bad_result_text}${bold_text}Unknown error${reset_text}'"
+    fi
+    echo "   ${bold_text}------> Begin output result for ${reset_text}'${highlight_text}${service_name}${reset_text}'"
+    echo "${openssl_connect_output}"
+    echo "   ${bold_text}<------ End output result for ${reset_text}'${highlight_text}${service_name}${reset_text}'"
   else
-    echo " ${bold_text}${bad_result_text}[Error]${reset_text}"
-    echo "   '${bad_result_text}${cmd_output}${reset_text}'"
+    openssl_check_output=$(echo "${openssl_connect_output}" |
+      openssl x509 -inform pem -noout -nocert -checkhost ${fqdn_to_check} 2>/dev/null) &&
+      exit_status=$? || exit_status=$?
+    if [[ $openssl_check_output =~ "does match certificate" ]]; then
+      echo " ${bold_text}${good_result_text}[OK]${reset_text}"
+      echo "   Valid certificate for '${good_result_text}${fqdn_to_check}${reset_text}' on port '${good_result_text}${port_to_check}${reset_text}'"
+    else
+      echo " ${bold_text}${bad_result_text}[Error]${reset_text}"
+      echo " ${highlight_text}Certificate for${reset_text} '${bold_text}${fqdn_to_check}:${port_to_check}${reset_text}' ${highlight_text}not valid${reset_text}"
+      echo "   '${bad_result_text}${openssl_check_output}${reset_text}'"
+    fi
   fi
 done
