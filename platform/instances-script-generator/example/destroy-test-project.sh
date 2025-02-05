97a9377ee5a7bfcf29eda051087ecc7a2cb2ba21
Duilio Ruggiero <duilio@sinetris.info>
2025-02-05 01:06:48 +0100

    Add check to wait for cloud-init in vbox example

--- platform/instances-script-generator/example/destroy-test-project.sh
+++ platform/instances-script-generator/example/destroy-test-project.sh
@@ -5,33 +5,33 @@ this_file_path=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 . "${this_file_path}/configuration.sh"
 
-instance_status=$(VBoxManage showvminfo "${vbox_instance_name:?}" --machinereadable 2>&1) \
-  && exit_code=$? || exit_code=$?
-if [[ $exit_code -eq 0 ]]; then
+_instance_status=$(VBoxManage showvminfo "${vbox_instance_name:?}" --machinereadable 2>&1) \
+  && _exit_code=$? || _exit_code=$?
+if [[ $_exit_code -eq 0 ]]; then
   echo "⚙️ Destroying instance '${vbox_instance_name:?}'!"
-  if [[ $instance_status =~ 'VMState="started"' ]] || [[ $instance_status =~ 'VMState="running"' ]]; then
+  if [[ $_instance_status =~ 'VMState="started"' ]] || [[ $_instance_status =~ 'VMState="running"' ]]; then
     VBoxManage controlvm "${vbox_instance_name:?}" poweroff
   fi
   VBoxManage unregistervm "${vbox_instance_name:?}" --delete-all
-elif [[ $exit_code -eq 1 ]] && [[ $instance_status =~ 'Could not find a registered machine' ]]; then
+elif [[ $_exit_code -eq 1 ]] && [[ $_instance_status =~ 'Could not find a registered machine' ]]; then
   echo "✅ Instance '${vbox_instance_name:?}' not found!"
 else
-  echo "❌ Instance '${vbox_instance_name:?}' - exit code '${exit_code}'"
-  echo ${instance_status}
+  echo "❌ Instance '${vbox_instance_name:?}' - exit code '${_exit_code}'"
+  echo ${_instance_status}
   exit 2
 fi
 
-project_network_status=$(VBoxManage hostonlynet modify \
-  --name ${project_network_name} --disable 2>&1) && exit_code=$? || exit_code=$?
-if [[ $exit_code -eq 0 ]]; then
+_network_status=$(VBoxManage hostonlynet modify \
+  --name ${project_network_name} --disable 2>&1) && _exit_code=$? || _exit_code=$?
+if [[ $_exit_code -eq 0 ]]; then
   echo "⚙️ Project Network '${project_network_name}' will be removed!"
   VBoxManage hostonlynet remove \
     --name ${project_network_name}
-elif [[ $exit_code -eq 1 ]] && [[ $project_network_status =~ 'does not exist' ]]; then
+elif [[ $_exit_code -eq 1 ]] && [[ $_network_status =~ 'does not exist' ]]; then
   echo "✅ Project Network '${project_network_name}' does not exist!"
 else
-  echo "❌ Project Network '${project_network_name}' - exit code '${exit_code}'"
-  echo ${project_network_status}
+  echo "❌ Project Network '${project_network_name}' - exit code '${_exit_code}'"
+  echo ${_network_status}
   exit 2
 fi
 
